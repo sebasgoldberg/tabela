7cd4224a18c51db80925385403df39823ae31611
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-12-19 11:49:16 -0200

    Configuração de extensão de controllers como replacement em lugar de extension de forma que seja possivel trabalhar com typed controllers.

---
+++ webapp/controller/TaskList.controller.js
@@ -0,0 +1,9 @@
+import formatter from 'simplifique/telaneg/tabela/model/formatter';
+import Controller from "simplifique/telaneg/custos/controller/TaskList.controller";
+
+export default Controller.extend("simplifique.telaneg.tabela.controller.TaskList", {
+
+    formatter: formatter,
+
+});
+
