f823d4ead56afd3dbbd6b2791d5120e896aa5f69
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2019-01-08 14:15:33 -0200

    Adição dos fragments para informação geral e abrangência.

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,8 +1,13 @@
 import formatter from 'simplifique/telaneg/custos/model/formatter';
 
 export default Object.assign({}, formatter, {
+
+    botaoNovoLabel: function(sTipoNegociacao){
+        return "Nova Tabela";
+    },
+
     apuracaoAteLabel: function(){
         return "Vigência Tabela";
-    }
+    },
 });
 
